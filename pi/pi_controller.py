8cd630a44b7df35daa68d829fe38a88f2f3c4b57
lemonmati <lemonmateusz@gmail.com>
2025-12-15 15:51:21 +0100

    works

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -2,38 +2,42 @@ import requests
 import time
 import random
 import click
+from sense_hat import SenseHat
+
+sense = SenseHat()
 
 
 def get_direction():
     d_long = 0
     d_la = 0
     send_vel = False
-    c = click.getchar()
-    if c =='a':
-        click.echo('Left')
-        send_vel = True
-        d_long = -1
-        d_la = 0
-    elif c == 'd':
-        click.echo('Right')
-        send_vel = True
-        d_long = 1
-        d_la = 0
-    elif c =='w':
-        click.echo('Up')
-        send_vel = True
-        d_long = 0
-        d_la = 1
-    elif c == 's':
-        click.echo('Down')
-        send_vel = True
-        d_long = 0
-        d_la = -1
-    else:
-        d_long = 0
-        d_la = 0
-        click.echo('Invalid input :(')
-        send_vel = False
+    for event in sense.stick.get_events():
+        if event.action == "pressed":        
+            if event.direction =='left':
+                click.echo('Left')
+                send_vel = True
+                d_long = -1
+                d_la = 0
+            elif event.direction == 'right':
+                click.echo('Right')
+                send_vel = True
+                d_long = 1
+                d_la = 0
+            elif event.direction =='up':
+                click.echo('Up')
+                send_vel = True
+                d_long = 0
+                d_la = 1
+            elif event.direction == 'down':
+                click.echo('Down')
+                send_vel = True
+                d_long = 0
+                d_la = -1
+            else:
+                d_long = 0
+                d_la = 0
+                click.echo('Invalid input :(')
+                send_vel = False
     return d_long, d_la, send_vel
 
 
